5a3809115e9762866b814bdc74c72091f8909eb1
flukeywheat <chapter_120@yahoo.com>
2019-05-29 10:31:00 -0500

    Trying to recover file

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const express = require("express");
-
-
+const passport = require("passport");
+const googleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
 
 const app = express();
 
 
 
-app.get("/", (req, res) =>{
-    res.sendfile(__dirname + "/public/index.html");
-})
+passport.use(new googleStrategy({
+    clientID: keys.client_id,
+    clientSecret: keys.private_key,
+    callbackURL: "http://localhost:3000/auth/google/callback"
+}, async (token, tokenSecret, profile, done) =>{
+    console.log(token);
+    
+    await(profile)
+}));
+
+
+app.get("/", passport.authenticate("google", {
+    scope: ["profile", "email"]
+    }) 
+);
 
 
 
